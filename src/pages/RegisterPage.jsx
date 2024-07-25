3e4ae4d8c9fa6bd8d1436b7a63dae465f735954f
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-07-25 17:56:56 -0300

    Setting register logic on the Register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../styles/Register.scss";
 
 const RegisterPage = () => {
@@ -22,10 +23,42 @@ const RegisterPage = () => {
 
     console.log(formData)
 
+    const [passwordMatch, setPasswordMatch] = useState(true)
+
+    useEffect(() => {
+        setPasswordMatch(formData.password === formData.confirmPassword || formData.confirmPassword === "")
+    })
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();       
+
+        try {
+            const register_form = new FormData()
+
+            for (var key in formData) {
+                register_form.append(key, formData[key])
+            }
+
+            const response = await fetch("http://localhost:3001/auth/register", {
+                method: 'POST',
+                body: register_form
+            })
+
+            if(response.ok){
+                navigate("/login");
+            }
+
+        } catch (err) {
+            console.log("Registration failed", err.message)
+        }
+    }
+
   return (
     <div className='register'>
         <div className="register_content">
-            <form className="register_content_form">
+            <form className="register_content_form" onSubmit={handleSubmit}>
                 <input
                     placeholder='First Name'
                     name="firstName"
@@ -64,6 +97,9 @@ const RegisterPage = () => {
                     value={formData.confirmPassword}
                     onChange={handleChange}
                 />
+                {!passwordMatch && (
+                    <p style={{ color: "red" }}>Passwords are not matched!</p>
+                )}
                 <input
                     id="image"
                     type='file'
@@ -86,7 +122,7 @@ const RegisterPage = () => {
                     />
                 )}
 
-                <button type='submit'>Register</button>
+                <button type='submit' disabled={!passwordMatch}>Register</button>
             </form>
             <a href='/login'>Already have an account? Login In Here</a>
         </div>
