da28e6231115f060fb4ffe45f7533c6a66f991a4
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-08-05 23:39:31 -0300

    Setting login logic and Login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "../styles/Login.scss";
+import { setLogin } from '../redux/state';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch("http://localhost:3001/auth/login", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      })
+
+      /* Get data after fetching */
+      const loggedIn = await response.json();
+      
+      if(loggedIn){
+        dispatch(
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token
+          })
+        )
+        navigate("/")
+      }
+
+    } catch (error) {
+      console.log("Login failed", error.message);
+    }
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className='login'>
+      <div className="login_content">
+        <form className='login_content_form' onSubmit={handleSubmit}>
+          <input
+            type='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Log In</button>
+        </form>
+        <a href="/">Don't have an account? Sign in Here</a>
+      </div>
+    </div>
   )
 }
 
